4193aed516428c89fddf8d2bcb21c29816a61a94
Precious Humphrey <phumphrey23@gmail.com>
2018-04-25 04:06:54 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(array)
-  array.detect {|element| element.capitalize + "!"}
+  array.collect {|element| element.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
