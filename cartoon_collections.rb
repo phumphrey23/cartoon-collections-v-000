5fde621146a8d9aa7ee56980c87d9e6dc2b26e6e
Precious Humphrey <phumphrey23@gmail.com>
2018-04-25 03:59:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)
   list_dwarves = []
-dwarves.each_with_index do |name, index|
+  dwarves.each_with_index do |name, index|
    new_dwarves << "#{index}. #{name}"
   end
   puts new_dwarves
