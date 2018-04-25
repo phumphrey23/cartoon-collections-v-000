2ee194e4959689c3db254fa99bbca1e5a40a1db8
Precious Humphrey <phumphrey23@gmail.com>
2018-04-25 04:19:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(calls)
-   calls.select {|call| call.length>4}
+   calls.any? {|call| call.length>4}
 end
 
 def find_the_cheese# code an argument here
